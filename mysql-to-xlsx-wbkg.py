cd480408c09669ea0078108851e4293e90323c61
albatros13 <natallia.kokash@gmail.com>
2023-07-27 18:19:12 +0200

    Added methods to override Spreadsheets from DB

--- mysql-to-xlsx-wbkg.py
+++ mysql-to-xlsx-wbkg.py
@@ -20,7 +20,6 @@ class MySQLApinatomyDB:
             row = dict(zip(col_names, item))
             row.pop('num_id')
             row['id'] = row.pop('ID')
-            # row['id'] = re.sub('\t+', '', row['id'])
             row['ontologyTerms'] = row.pop('ontologyTerm')
             row['isTemplate'] = 'TRUE' if row.pop('isTemplate') == 1 else ''
             row['name'] = row.pop('label')
@@ -61,26 +60,34 @@ class MySQLApinatomyDB:
         cursor = self.driver.cursor()
         for row in rows_to_insert:
             row["isTemplate"] = 1 if row['isTemplate'] == 'TRUE' else 0
+            if "color" in row:
+                del row["color"]
         rows = [tuple(d.values()) for d in rows_to_insert]
-        # WS: id,ontologyTerms,name,isTemplate,topology,layers,supertype,internalLyphs,internalLyphsInLayers,hostedBy
-        sql = "INSERT INTO lyphs (ID, ontologyTerm, label, isTemplate, topology, layers, supertype, hostedBy, " \
-              "internalLyphs, internalLyphsInLayers) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        # WS: id,ontologyTerms,name,varianceSpecs,isTemplate,topology,layers,supertype,internalLyphs,internalLyphsInLayers,hostedBy
+
+        sql = "INSERT INTO lyphs ({0}) VALUES ({1})".format(LYPH_COLUMNS_STR, LYPH_VARS_STR)
         cursor.executemany(sql, rows)
         self.driver.commit()
         cursor.close()
 
     def insert_materials(self, rows_to_insert):
         cursor = self.driver.cursor()
+        for row in rows_to_insert:
+            if "color" in row:
+                del row["color"]
         rows = [tuple(d.values()) for d in rows_to_insert]
-        sql = "INSERT INTO materials (id, ontologyTerms, name, materials) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO materials ({0}) VALUES ({1})".format(MATERIAL_COLUMNS_STR, MATERIAL_VARS_STR)
         cursor.executemany(sql, rows)
         self.driver.commit()
         cursor.close()
 
     def insert_chains(self, rows_to_insert):
         cursor = self.driver.cursor()
+        for row in rows_to_insert:
+            if "color" in row:
+                del row["color"]
         rows = [tuple(d.values()) for d in rows_to_insert]
-        sql = "INSERT INTO chains (id, name, lyphs, wiredTo) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO chains ({0}) VALUES ({1})".format(CHAIN_COLUMNS_STR, CHAIN_VARS_STR)
         cursor.executemany(sql, rows)
         self.driver.commit()
         cursor.close()
@@ -89,8 +96,8 @@ class MySQLApinatomyDB:
         if len(dataset) == 0:
             return
         cursor = self.driver.cursor()
-        sql = "DELETE FROM {0} WHERE id in ({1})".format(name, ",".join(f"'{w}'" for w in dataset))
-        cursor.execute(sql)
+        sql = "DELETE FROM %s WHERE id in (%s)"
+        cursor.execute(sql, (name, ",".join(f"'{w}'" for w in dataset)))
         self.driver.commit()
         cursor.close()
 
@@ -106,8 +113,8 @@ class MySQLApinatomyDB:
                 val = row['WS']
                 if col == 'isTemplate':
                     val = 1 if row['WS'] == 'TRUE' else 0
-                sql = "UPDATE lyphs SET {0} = '{1}' WHERE ID = '{2}'".format(col, val, row['ID'])
-                cursor.execute(sql)
+                sql = "UPDATE lyphs SET {0} = %s WHERE ID = %s".format(col)
+                cursor.execute(sql, (val, row['ID']))
         self.driver.commit()
         cursor.close()
 
@@ -119,8 +126,8 @@ class MySQLApinatomyDB:
             for row in entries:
                 col = 'lyph_sequence' if row['COL'] == 'lyphs' else 'label' if row['COL'] == 'name' else \
                     'wire_id' if row['COL'] == 'wiredTo' else row['COL']
-                sql = "UPDATE chains SET {0} = '{1}' WHERE ID = '{2}'".format(col, row['WS'], row['ID'])
-                cursor.execute(sql)
+                sql = "UPDATE chains SET {0} = %s WHERE ID = %s".format(col)
+                cursor.execute(sql, (row['WS'], row['ID']))
         self.driver.commit()
         cursor.close()
 
@@ -130,8 +137,8 @@ class MySQLApinatomyDB:
         cursor = self.driver.cursor()
         for entries in dataset:
             for row in entries:
-                sql = "UPDATE materials SET {0} = '{1}' WHERE ID = '{2}'".format(row['COL'], row['WS'], row['ID'])
-                cursor.execute(sql)
+                sql = "UPDATE materials SET {0} = %s WHERE ID = %s".format(row['COL'])
+                cursor.execute(sql, (row['WS'], row['ID']))
         self.driver.commit()
         cursor.close()
 
@@ -175,15 +182,17 @@ def create_local_excel(df_lyphs, df_chains, df_materials, file_path):
 
 FILE_NAME = './data/wbkg2.xlsx'
 
-# login = json.load(open('./data/wbkg_db.json', 'r'))
-login = json.load(open('./data/wbkg_db_nk.json', 'r'))
+login = json.load(open('./data/wbkg_db.json', 'r'))
+# login = json.load(open('./data/wbkg_db_nk.json', 'r'))
 
 db_name, db_user, db_pwd, db_server, db_port = (login.values())
 mysql_db = MySQLApinatomyDB(db_name, db_user, db_pwd, db_server, db_port)
 
 gc = gspread.service_account(filename='./data/service_account.json')
-# sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1BcUBExy-kk-03ceeFuuz8comX3-O0xL1_owTkmZQTCU/edit#gid=1006273879")
-sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1h1uO8BZ6BWt55YPZqs0TOJZ8LeughISsnlcDgEw1f_U/edit#gid=1006273879")
+sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1BcUBExy-kk-03ceeFuuz8comX3-O0xL1_owTkmZQTCU/edit#gid=1006273879")
+# sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Y1nPCiRTiM1A8xUQn8NwNOlsz7z32hGd6W6U_ZQOrLY/edit#gid=1006273879")
+# sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1h1uO8BZ6BWt55YPZqs0TOJZ8LeughISsnlcDgEw1f_U/edit#gid=1006273879")
+# sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1jDtLrfm1Kz25oAcjRGEFu3qXqh5iz1_4lAEaQzuHQDc/edit?pli=1#gid=855732402")
 
 
 # Replaces the spreadsheet completely (not a good option as we lose editing history
@@ -300,6 +309,7 @@ def update_in_db(dataset, name):
     if name == 'materials':
         mysql_db.update_materials(dataset)
 
+
 # Compare worksheet with DB table
 def compare_sheet(df, name):
     db_records = df.to_dict('records')
@@ -382,14 +392,30 @@ def compare_sheet(df, name):
 
 yes_to_all = False
 
+LYPH_COLUMNS = ['id', 'ontologyTerms', 'name', 'varianceSpecs', 'isTemplate', 'topology', 'layers',
+                         'supertype', 'internalLyphs', 'internalLyphsInLayers', 'hostedBy']
+CHAIN_COLUMNS = ['id', 'name', 'lyphs', 'wiredTo']
+MATERIAL_COLUMNS = ['id', 'ontologyTerms', 'name', 'materials']
+
+LYPH_COLUMNS_STR = ', '.join(LYPH_COLUMNS)
+CHAIN_COLUMNS_STR = ', '.join(CHAIN_COLUMNS)
+MATERIAL_COLUMNS_STR = ', '.join(MATERIAL_COLUMNS)
+
+print(LYPH_COLUMNS_STR)
+print(CHAIN_COLUMNS_STR)
+print(MATERIAL_COLUMNS_STR)
+
+LYPH_VARS_STR = ', '.join(['%s' for x in range(len(LYPH_COLUMNS))])
+CHAIN_VARS_STR = ', '.join(['%s' for x in range(len(CHAIN_COLUMNS))])
+MATERIAL_VARS_STR = ', '.join(['%s' for x in range(len(MATERIAL_COLUMNS))])
+
 # Lyphs
 action = 'y' if yes_to_all else input("Compare lyphs (y)?")
 if action == 'y':
     df_lyphs = pd.DataFrame.from_records(mysql_db.query_lyphs())
     # df_lyphs = pd.read_excel(FILE_NAME, sheet_name='lyphs',dtype=str)
     # df_lyphs = df_lyphs.replace(np.nan, '')
-    df_lyphs = df_lyphs[['id', 'ontologyTerms', 'name',	'isTemplate', 'topology', 'layers',	'supertype', 'internalLyphs',
-                         'internalLyphsInLayers', 'hostedBy']]
+    df_lyphs = df_lyphs[LYPH_COLUMNS]
     lyphs = df_lyphs.values.tolist()
     compare_sheet(df_lyphs, "lyphs")
 
@@ -397,6 +423,7 @@ if action == 'y':
 action = 'y' if yes_to_all else input("Compare chains (y)?")
 if action == 'y':
     df_chains = pd.DataFrame.from_records(mysql_db.query_chains())
+    df_chains = df_chains[CHAIN_COLUMNS]
     # df_chains = pd.read_excel(FILE_NAME, sheet_name='chains',dtype=str)
     # df_chains = df_chains.replace(np.nan, '')
     chains = df_chains.values.tolist()
@@ -406,6 +433,7 @@ if action == 'y':
 action = 'y' if yes_to_all else input("Compare materials (y)?")
 if action == 'y':
     df_materials = pd.DataFrame.from_records(mysql_db.query_materials())
+    df_materials = df_materials[MATERIAL_COLUMNS]
     # df_materials = pd.read_excel(FILE_NAME, sheet_name='materials',dtype=str)
     # df_materials = df_materials.replace(np.nan, '')
     materials = df_materials.values.tolist()
