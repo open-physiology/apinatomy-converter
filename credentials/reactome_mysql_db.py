708c41acffba9551fe36e2be98ecda48c5c8600b
albatros13 <natallia.kokash@gmail.com>
2024-06-27 18:19:51 +0200

    Removed unused data

--- credentials/reactome_mysql_db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-driver = mysql.connector.connect(
-    user="uvxtlrmq1fxv4",
-    password="XXX",
-    database="db3uzqasmtygtv",
-    host="35.214.220.45",
-    port="3306")
