838ed1a93026bc0db6deb328cd7f602775f63ab0
Natallia Kokash <natallia.kokash@gmail.com>
2024-06-27 18:13:25 +0200

    Update reactome_mysql_db.py

--- credentials/reactome_mysql_db.py
+++ credentials/reactome_mysql_db.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 driver = mysql.connector.connect(
     user="uvxtlrmq1fxv4",
-    password="*84m@@conb1U",
+    password="XXX",
     database="db3uzqasmtygtv",
     host="35.214.220.45",
     port="3306")
